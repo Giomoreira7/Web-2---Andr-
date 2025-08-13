827d26b12f837f0d8b88b1306fbddc0444b818eb
Giomoreira7 <gi.ovannamoreiradasilva2710@gmail.com>
2025-08-12 22:39:38 -0300

    g

--- Manflix/app/urls.py
+++ Manflix/app/urls.py
@@ -1,11 +1,10 @@
 from .views import *
-
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
-router.register (r'movies',MoviesView)
-router.register (r'directors',MoviesView)
-router.register (r'plans',MoviesView)
+router.register(r'directors',DirectorsView)
+router.register(r'movies',MoviesView)
+router.register(r'plans',PlansView)
 
 urlpatterns = router.urls
 
